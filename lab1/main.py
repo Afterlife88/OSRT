f315369077f5739945585856ce17b1ccc588bad0
Andrey Bulat <andrii.bulat@gmail.com>
2017-02-28 21:09:09 +0200

    Added functions to test series convergence

--- lab1/main.py
+++ lab1/main.py
@@ -1,4 +1,40 @@
-from lab1.modules import series
+import lab1.modules.series as series
 
-n = input("Enter n: ")
-print (series.sumOfSeries(n))
+series_name = "(n/n+1)^(n^2)"
+e = float(input("Input e (acceptable error): "))
+n = int(input("Input n (max element index to check for): "))
+
+series.max_n = n
+
+limits_on = series.check_limit(e)
+
+print("Checking necessary conditions for convergent series")
+
+limits = False
+
+if limits_on != -1:
+    print("Series " + series_name + " limits on " + str(limits_on) + "th element. Continue testing")
+    limits = True
+else:
+    print("Series " + series_name + " does not limits to " + str(e) + " at first " + str(series.max_n) +
+          " elements. Not convergent series.")
+
+print("\n")
+
+if limits:
+    print("Checking ratio test")
+    result = series.ratio_test(e)
+
+    if result > 1:
+        natural_result = "divergent"
+    elif result < 1:
+        natural_result = "convergent"
+    else:
+        natural_result = "uncertainty"
+
+    print("Result for " + series_name + " in ratio test is q=" + str(result) + ". Series is " + natural_result)
+
+print("\n")
+
+print("Ending script. Thank you for using 'Not useful at all 2k17 series tester © IT41-group5'")
+
--- lab1/modules/series.py
+++ lab1/modules/series.py
@@ -3,8 +3,44 @@ from decimal import Decimal
 
 # Series module
 
-def sumOfSeries(n):
-    sum = Decimal(0.0)
-    for i in range(1, n+1):
-        sum += Decimal(pow(i / (i + 1), pow(i, 2)))
-    return Decimal(sum)
+
+max_n = 10000
+
+
+def get_nth(n):
+    return pow(n / (n + 1), pow(n, 2))
+
+
+def check_limit(e):
+    i = 1
+    n = e + 1.0
+    limits_on = -1
+
+    while abs(n) >= e and i <= max_n:
+        n = get_nth(i)
+        i += 1
+
+    if abs(n) < e:
+        limits_on = i - 1
+
+    return limits_on
+
+
+def ratio_test(e):
+    def divide(i):
+        return get_nth(i + 1) / get_nth(i)
+
+    i = 2
+
+    q_n = divide(1)
+    q_n_1 = divide(2)
+
+    while abs(q_n_1 - q_n) >= e and abs(1 - q_n_1) >= e and i <= max_n:
+        q_n = q_n_1
+        q_n_1 = divide(i)
+        i +=1
+
+    if i > max_n or abs(1 - q_n_1) < e:
+        return 1
+    else:
+        return q_n_1
